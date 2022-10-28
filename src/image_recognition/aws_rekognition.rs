1a44dba2c221be6a94ca1e2177476a1c6a2c1b90
ppoliani <ppoliani@gmail.com>
2022-10-28 09:39:45 +0300

    feat: add min confidence

--- src/image_recognition/aws_rekognition.rs
+++ src/image_recognition/aws_rekognition.rs
@@ -24,15 +24,18 @@ impl AwsRekognition {
     Self {client}
   }
 
-  pub async fn recognise(&self, image_content: Vec<u8>) -> Result<Option<Vec<ModerationLabel>>> {
+  pub async fn recognise(&self, image_content: Vec<u8>, min_confidence: Option<f32>) -> Result<Option<Vec<ModerationLabel>>> {
     // this allows 5MB images; for larger images use `s3_object`. This requires that the image is already present in an S3 bucket.
     let image = Image::builder()
     .set_bytes(Some(Blob::new(image_content)))
     .build();
 
+    // If you don't specify the MinConfidence parameter in the call to DetectModerationLabels, the operation returns labels
+    // with a confidence value greater than or equal to 50 percent.
     let moderations_labels = self.client
     .detect_moderation_labels()
     .set_image(Some(image))
+    .set_min_confidence(min_confidence)
     .send()
     .await?
     .moderation_labels()
